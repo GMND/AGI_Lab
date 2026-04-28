8f05f96b832c09de5b6a5960f6dee8d61ca1e031
GMND <gmnd.link@gmail.com>
2026-04-28 14:14:58 +0300

    update

--- experiments/nshc_prototype/episodic_tree.py
+++ experiments/nshc_prototype/episodic_tree.py
@@ -1,1 +1,7 @@
 import numpy as np
+class EpisodicNode:
+    def __init__(self, content, embedding, importance_score=0.0):
+        self.content = content
+        self.embedding = np.array(embedding)
+        self.importance_score = importance_score
+        self.children = []
